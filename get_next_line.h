64e083cbdd778348d3d405361db620505f416b22
Dar <noemeuniercoli@gmail.com>
2025-12-01 11:00:14 +0100

    zzz

---
+++ get_next_line.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmeunier <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/27 13:57:46 by nmeunier          #+#    #+#             */
+/*   Updated: 2025/11/27 13:57:46 by nmeunier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+
+
+char	*get_next_line(int fd);
+
+
+#endif
